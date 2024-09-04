e9ae5fd7ce53752f0b7af3af981f04e7dcc5a825
eljudepanao <eljudepanao03@gmail.com>
2024-09-04 09:07:30 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  const [qrCode, setQrCode] = useState("")
+  const [inputError, setInputError] = useState(false)
+  const [errorMessage, setErrorMessage] = useState("")
+
+  const notifySuccess = () => {
+    toast("Successfully Generated!");
+  }
+
+  const notifyError = (message) => {
+    toast.error(message);
+  }
+
+  const handleError = (url) => {
+    try {
+      new URL(url);
+      return true;
+    } catch (_) {
+      return false;
+    }
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const url = e.target[0].value
+
+    if (!handleError(url)) {
+      setInputError(true)
+      setErrorMessage("Invalid URL. Please enter a valid URL.")
+      notifyError("Invalid URL. Please enter a valid URL.");
+      return;
+    }
+
+    setInputError(false)
+    setErrorMessage("")
+
+    try {
+      const response = await fetch(
+        `https://qr-generator-0215.azurewebsites.net/api/GenerateQR?url=${url}`,
+        { method: "GET", headers: { "Content-Type": "application/json" }}
+      );
+
+      if(response.ok) {
+        const data = await response.json()
+        setQrCode(data.qr_code_url)
+        console.log(data)
+        notifySuccess()
+      }
+
+    } catch (error) {
+      console.log(error)
+      notifyError("An error occurred while generating the QR code. Please try again.");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+     <div className="App">
+        <h2>QR Code Generator</h2>
+        <form action="" onSubmit={handleSubmit}>
+          <input 
+            type="text" 
+            placeholder="Enter a URL" 
+            style={{ borderColor: inputError ? 'red' : 'initial' }} 
+          />
+          {inputError && <p style={{ color: 'red' }}>{errorMessage}</p>}
+          <button type="submit">Generate</button>
+        </form>
+
+        {qrCode && (<div className='qrCode'>
+          <img src={qrCode} alt="qrCode" />
+        </div>)}
+        <ToastContainer />
+     </div>
+    </>
+  )
 }
 
-export default App;
+export default App
